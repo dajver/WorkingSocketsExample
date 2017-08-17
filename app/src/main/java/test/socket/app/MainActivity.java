c7165c5b45e158762b195f38656a8aec841f5fb7
gleb <lfqdth5@gmail.com>
2017-08-18 00:48:59 +0300

    added socket server, have made echo for it have added new method to call socket in heroku

--- app/src/main/java/test/socket/app/MainActivity.java
+++ app/src/main/java/test/socket/app/MainActivity.java
@@ -1,33 +1,32 @@
 package test.socket.app;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import test.socket.app.tasks.ConnectSocketTask;
-import test.socket.app.tasks.SendMessageTask;
+import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, SendMessageTask.OnSendPostExecuteListener, ConnectSocketTask.OnConnectPostExecuteListener {
+public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "test.socket.app";
+    private static final String HOST = "ws://stark-caverns-76076.herokuapp.com";
 
-    @BindView(R.id.button)
-    Button button;
-    @BindView(R.id.button2)
-    Button connect;
-    @BindView(R.id.textView)
-    TextView textView;
     @BindView(R.id.editText)
     EditText editText;
+    @BindView(R.id.textView)
+    TextView textView;
 
-    private boolean connected = false;
-    private SendMessageTask sendMessage;
-    private ConnectSocketTask connectSocketTask;
+    private WebSocketClient mWebSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,31 +34,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        button.setOnClickListener(this);
-        connect.setOnClickListener(this);
+        connectWebSocket();
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button:
-                sendMessage = new SendMessageTask(connected, connectSocketTask.getSocket(), this);
-                sendMessage.execute(editText.getText().toString());
-                break;
-            case R.id.button2:
-                connectSocketTask = new ConnectSocketTask(this);
-                connectSocketTask.execute();
-                break;
+    private void connectWebSocket() {
+        try {
+            URI uri = new URI(HOST);
+            mWebSocketClient = new WebSocketClient(uri) {
+                @Override
+                public void onOpen(ServerHandshake serverHandshake) {
+                    mWebSocketClient.send("Connecting from " + Build.MANUFACTURER + " " + Build.MODEL);
+                }
+
+                @Override
+                public void onMessage(String s) {
+                    final String message = s;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(message);
+                        }
+                    });
+                }
+
+                @Override
+                public void onClose(int i, String s, boolean b) {
+                    Log.i("Websocket", "Connection Closed " + s);
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    e.printStackTrace();
+                }
+            };
+            mWebSocketClient.connect();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
         }
     }
 
-    @Override
-    public void onSendPostExecute(String text) {
-        textView.setText(text);
-    }
-
-    @Override
-    public void onConnectPostExecute() {
-        connect.setVisibility(View.GONE);
+    @OnClick(R.id.button)
+    public void onSendClick() {
+        mWebSocketClient.send(editText.getText().toString());
+        editText.setText("");
     }
 }
