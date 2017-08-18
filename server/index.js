90af0bc2ba6015d9f403c065ecb98b92731b2e5b
gleb <lfqdth5@gmail.com>
2017-08-18 13:28:12 +0300

    added server code

---
+++ server/index.js
@@ -0,0 +1,22 @@
+var WebSocketServer = require('ws').Server
+  , http = require('http')
+  , express = require('express')
+  , app = express()
+  , port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/'));
+
+var server = http.createServer(app);
+server.listen(port);
+
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        ws.send("Server received: " + message, function() {  });
+    });
+
+    ws.on('close', function() {
+        console.log('websocket connection close');
+        connections = new Map();
+    });
+});
